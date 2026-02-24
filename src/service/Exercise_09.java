c2be82faa799e8b454436b337c252d08e0a5f565
ValdisEizans <valdis.eizans@gmail.com>
2026-02-24 22:49:26 +0200

    Exercise 9 updated

--- src/service/Exercise_09.java
+++ src/service/Exercise_09.java
@@ -3,8 +3,31 @@ package service;
 public class Exercise_09 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String virkne = "[1, 2, 3] + [3, 5, 7]";
+		
+		if(virkne.contains("+")) {
+			String[] masivs = virkne.split("\\+");
+			masivs[0] = masivs[0].replace('[', ' ');
+			masivs[0] = masivs[0].replace(']', ' ');
+			masivs[1] = masivs[1].replace('[', ' ');
+			masivs[1] = masivs[1].replace(']', ' ');
+			String[] virkne1 = masivs[0].split(", ");
+			String[] virkne2 = masivs[1].split(", ");
+			
+			for (String s : virkne1) {
+				  System.out.println(s);
+				}
+			for (String s : virkne2) {
+				  System.out.println(s);
+				}
+			}
+		
+		
+				
 
+		
+		
+		
 	}
 
 }
